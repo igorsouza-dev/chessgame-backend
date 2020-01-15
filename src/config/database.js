c2e4d02e27ed6dcfd4b99699c4b2dfed8f9321f0
Igor Souza <igorsouza.dev@gmail.com>
2020-01-14 21:25:20 -0300

    Added branch develop

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
 
 module.exports = {
+  production: {
+    dialect: 'sqlite',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    storage: './src/database/db/chess.sqlite3',
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
   development: {
     dialect: 'sqlite',
     host: process.env.DB_HOST,
